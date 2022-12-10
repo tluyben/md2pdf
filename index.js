b6467a1336050a7c4a6cd52bbb110e6d69defc76
Tycho Luyben <tycho@e-lab.nl>
2022-12-10 11:13:01 +0000

    cutting long lines

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const fs = require("fs");
 const path = require("path");
 const mdpdf = require('mdpdf');
 
+const MAX_LINE_LENGTH = 150;
 
 // Define the function to scan a directory for PHP files and extract substrings between balanced braces
 async function scanDirectory(dir) {
@@ -27,12 +28,37 @@ async function scanDirectory(dir) {
     return contents
 }
 
+// function to cut long lines into smaller lines
+function cutLongLines(text) {
+    let lines = text.split('\n');
+    let newLines = [];
+    for (let line of lines) {
+        if (line.length > MAX_LINE_LENGTH) {
+            let newLine = '';
+            let words = line.split(' ');
+            for (let word of words) {
+                if (newLine.length + word.length > MAX_LINE_LENGTH) {
+                    newLines.push(newLine);
+                    newLine = '';
+                }
+                newLine += word + ' ';
+            }
+            newLines.push(newLine);
+        } else {
+            newLines.push(line);
+        }
+    }
+    return newLines.join('\n');
+}
+
+
 // Get the directory path from the command line arguments
 const dir = process.argv[2];
 
 (async () => {
     // Scan the dir
-    const result = await scanDirectory(dir);
+    const result = cutLongLines(await scanDirectory(dir));
+
 
     // generate a random number
     const randomNumber = Math.floor(Math.random() * 1000000000) + 1;
@@ -45,9 +71,10 @@ const dir = process.argv[2];
     let options = {
         source: tmpFile,
         destination: path.join(__dirname, 'output.pdf'),
-        //styles: path.join(__dirname, 'md-styles.css'),
+        //styles: path.join(__dirname, 'style.css'),
         pdf: {
             format: 'A4',
+            //ghStyle: true,
             orientation: 'portrait'
         }
     };
